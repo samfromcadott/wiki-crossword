c3d0f28476d7f4275ec881efd832afb5f93872f3
Sam Jackson <samahstan@gmail.com>
2021-01-26 10:00:22 -0600

    Added the clue generator I made yesterday

---
+++ backend/make_clue.py
@@ -0,0 +1,22 @@
+import wikipediaapi
+import re
+
+wiki_en = wikipediaapi.Wikipedia('en')
+
+def makeClue(page):
+	expresion = re.compile("(?:is|was|are|were)(.*?[\.!\?](?:\s|$))")
+	page = wiki_en.page(page)
+	summary = ""
+
+	if page.exists():
+		summary = page.summary
+	else:
+		return None
+
+	clue = expresion.search(summary).group(1)
+	clue = clue.strip()
+	clue = clue[:1].upper() + clue[1:]
+
+	return clue
+
+print(makeClue('Cadott, Wisconsin'))
