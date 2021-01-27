7407a46722263304a488b4ffb8101d7e6917aeb5
Sam Jackson <samahstan@gmail.com>
2021-01-27 10:51:48 -0600

    Basic crossword rendering

--- frontend/puzzle.js
+++ frontend/puzzle.js
@@ -1,9 +1,62 @@
-var puzzle = new Vue({
+var puzzle = {
+	width: 10,
+	height: 10,
+
+	across: {
+		1: {
+			x: 0,
+			y: 0,
+			answer: 'abc'
+		},
+		2: {
+			x: 3,
+			y: 6,
+			answer: 'abcd'
+		}
+
+	},
+
+	down: {
+		1: {
+			x: 0,
+			y: 0,
+			answer: 'abc'
+		},
+		2: {
+			x: 4,
+			y: 4,
+			answer: 'abcd'
+		}
+
+	}
+}
+
+var grid = new Array(puzzle.height);
+
+for (var i = 0; i < grid.length; i++) {
+	grid[i] = new Array(puzzle.width);
+
+}
+
+for (word in puzzle.across) {
+	var w = puzzle.across[word]
+	for (var i = 0; i < w.answer.length; i++) {
+		grid[w.y][w.x+i] = w.answer[i]
+	}
+}
+
+for (word in puzzle.down) {
+	var w = puzzle.down[word]
+	for (var i = 0; i < w.answer.length; i++) {
+		grid[w.y+i][w.x] = w.answer[i]
+	}
+}
+
+var crossword = new Vue({
 	el: '#puzzle',
 	data: {
-		items: [
-			{ message: 'Foo' },
-			{ message: 'Bar' }
-		]
+		grid: grid
+
 	}
+
 })
