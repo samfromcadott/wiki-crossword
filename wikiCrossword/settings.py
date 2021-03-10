22291ea8fe601d2824f843d0c6fa6305b4723ff2
Luke Bell <luke.d.bell1@gmail.com>
2021-03-09 21:25:01 -0600

    Cleaning up settings.py

--- wikiCrossword/settings.py
+++ wikiCrossword/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'k$9u523+%ya8(*#970hb8r3xf3ztbx3tj!(q2s#9rbo0xr8^i*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['wiki-crossword-dev.herokuapp.com','https://wiki-crossword.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,9 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_DIRS = [
-    BASE_DIR / "wikiCrossword/static"
-]
